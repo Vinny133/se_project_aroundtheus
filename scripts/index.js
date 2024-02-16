f935786fbcd6c3a33e3d73c007d6374fa4eb6855
Vinny <vinicius.porcella@gmail.com>
2024-02-16 12:55:47 -0500

    edit button can be opened and closed

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,15 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditBtn = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileModalClose = document.querySelector("#profile-modal-close");
+
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileModalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
