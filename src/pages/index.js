3e599a56e65cf5d3bc44508af31af53c08c51212
Vinny <vinicius.porcella@gmail.com>
2024-06-06 19:32:53 -0400

    fixed file name

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import Section from "../components/Section.js";
 import UserInfo from "../components/UserInfo.js";
-import Api from "../components/API.js";
+import Api from "../components/Api.js";
 import {
   initialCards,
   cardListEl,
